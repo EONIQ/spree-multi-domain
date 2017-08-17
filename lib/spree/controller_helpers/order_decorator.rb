7f00ff9453d991908f9edfe4f457688caf185a78
chung.triniti@gmail.com <johndoe@example.com>
2017-08-17 17:19:48 +0800

    Add by store cart

--- lib/spree/controller_helpers/order_decorator.rb
+++ lib/spree/controller_helpers/order_decorator.rb
@@ -8,4 +8,24 @@ Spree::Core::ControllerHelpers::Order.class_eval do
     currency = Spree::Config[:currency] if currency.blank?
     currency
   end
+
+  def find_order_by_token_or_user(options={}, with_adjustments = false)
+    options[:lock] ||= false
+
+    # Find any incomplete orders for the guest_token
+    incomplete_orders = Spree::Order.incomplete.by_store(Spree::Store.current_store).includes(line_items: [variant: [:images, :option_values, :product]])
+    guest_token_order_params = current_order_params.except(:user_id)
+    order = if with_adjustments
+              incomplete_orders.includes(:adjustments).lock(options[:lock]).find_by(guest_token_order_params)
+            else
+              incomplete_orders.lock(options[:lock]).find_by(guest_token_order_params)
+            end
+
+    # Find any incomplete orders for the current user
+    if order.nil? && try_spree_current_user
+      order = last_incomplete_order
+    end
+
+    order
+  end
 end
